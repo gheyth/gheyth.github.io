571319ef463f868ee77c79963d29ba747cd79dfd
fares <djefaflia.fares@gmail.com>
2022-08-14 11:26:44 +0100

    fix general

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -8,3 +8,18 @@ navOpen.addEventListener('click', () => {
   list.classList.toggle("list-nav-resp-open")
 })
   //////////////
+
+let LinkNavbar = document.querySelectorAll(".list-nav-resp li a");
+for (i = 0; i < LinkNavbar.length; i++){
+  LinkNavbar[i].addEventListener("click", () => {
+    navOpen.children[0].classList.toggle("hidden")
+    navOpen.children[1].classList.toggle("hidden")
+    list.classList.toggle("list-nav-resp-open")
+  })
+}
+
+function NavbarResponsive() {
+  navOpen.children[0].classList.toggle("hidden")
+  navOpen.children[1].classList.toggle("hidden")
+  list.classList.toggle("list-nav-resp-open")
+}
